e6455dc54dd32233c79e7f2c89e6110dd40f0618
Sean P <codeMonkey760@hotmail.com>
2016-02-17 13:58:00 -0800

    Began work on cubelets

--- src/Headers/Cubelet.h
+++ src/Headers/Cubelet.h
@@ -1,4 +1,15 @@
 #ifndef _C_RUBE_CUBELET_H_
 #define _C_RUBE_CUBELET_H_
 
+typedef struct {
+    float posW[3];
+    float rotation[4]; // rotation internally represented by a quaternion
+    float scale[3];
+    
+} Cubelet;
+
+void InitCubeletArray(Cubelet *array);
+void DrawCubeletArray(Cubelet *array, int numCubelets, Camera *cam);
+void BuildMatricies(Cubelet *singleCubelet, float wMtx[16], float witMtx[16]);
+
 #endif
