95e38941d99596da495c7089e4b67cb90cb84e60
Sean P <codeMonkey760@hotmail.com>
2016-03-05 12:41:33 -0800

    FEATURE: Added support for GUI push buttons.

--- src/Headers/GUIButton.h
+++ src/Headers/GUIButton.h
@@ -1,4 +1,45 @@
 #ifndef _C_RUBE_GUIBUTTON_H_
 #define _C_RUBE_GUIBUTTON_H_
 
+typedef void (*onClickFunc) (int,bool);
+typedef void (*onHighlightFunc) (void);
+
+typedef struct {
+    bool visible;
+    bool enabled;
+    bool depressed;
+    bool highlighted;
+    onClickFunc onClick;          // Button click callback
+    onHighlightFunc onHighlight;  // Button highlight callback
+    bool mouseDownInBounds;
+    int sliceId;
+    bool sliceForward;
+    
+    float posPercent[3];  //Position of the button's center in screen percentage
+    float dim[3];         // Dimensions of the button in screen percent
+    float rot;
+    
+    float tMtx[16];
+    int texId;
+    
+    float ambientColor[3];
+    float mixValue;
+} GUIButton;
+
+void InitButton (GUIButton *button);
+void SetPRD (
+    GUIButton *button, 
+    float percentX, 
+    float percentY, 
+    float percentW, 
+    float percentH, 
+    float rotZ
+);
+void _RecalcTMtx (GUIButton *button);
+bool inBounds (GUIButton *button, float percentX, float percentY);
+void ButtonOnMouseUp (GUIButton *button);
+void ButtonOnMouseDown (GUIButton *button);
+void ButtononMouseEnter (GUIButton *button);
+void ButtononMouseExit (GUIButton *button);
+
 #endif
---
+++ src/Source/GUIButton.c
@@ -0,0 +1,137 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+#include "Util.h"
+#include "GUIButton.h"
+
+void InitButton (GUIButton *button) {
+    if (button == NULL) return;
+    
+    memset(button,0,sizeof(GUIButton));
+    
+    button->visible = true;
+    button->enabled = true;
+    button->sliceForward = true;
+    
+    button->dim[0] = 1.0f;
+    button->dim[1] = 1.0f;
+    button->dim[2] = 1.0f;
+    
+    button->tMtx[16];
+    button->texId;
+    
+    button->mixValue = 0.7f;
+}
+
+void SetPRD (
+    GUIButton *button,
+    float percentX, 
+    float percentY, 
+    float percentW, 
+    float percentH, 
+    float rotZ
+) {
+    if (button == NULL) return;
+    
+    button->posPercent[0] = percentX;
+    button->posPercent[1] = percentY;
+    button->dim[0] = percentW;
+    button->dim[1] = percentH;
+    button->rot = rotZ;
+    
+    _RecalcTMtx(button);
+}
+
+void _RecalcTMtx (GUIButton *button) {
+    if (button == NULL) return;
+    
+    float sMtx[16] = {0.0f};
+    float rMtx[16] = {0.0f};
+    float tMtx[16] = {0.0f};
+    float scale[3] = {1.0f,1.0f,1.0f};
+    
+    Mat4Scaling(sMtx,button->dim);
+    Mat4RotationZ(rMtx,button->rot);
+    Mat4Translation(tMtx, button->posPercent);
+    
+    //NOTE: This is wrong
+    Mat4Mult(sMtx, rMtx, button->tMtx);
+    Mat4Mult(tMtx, button->tMtx, button->tMtx);
+}
+
+bool InBounds (GUIButton *button, float x, float y) {
+    if (button == NULL) return false;
+    
+    float hw = button->dim[0] / 2.0f;
+    float hh = button->dim[1] / 2.0f;
+    
+    float x1 = button->posPercent[0] - hw;
+    float y1 = button->posPercent[1] - hh;
+    float x2 = button->posPercent[0] + hw;
+    float y2 = button->posPercent[1] + hh;
+    
+    if (
+        x1 < x && x < x2 &&
+        y1 < y && y < y2 &&
+        button->visible == true &&
+        button->enabled == true
+    ) {
+        return true;
+    }
+    
+    return false;
+}
+
+void ButtonOnMouseUp (GUIButton *button) {
+    if (button == NULL) return;
+    
+    if (button->visible == false || button->enabled == false) return;
+    
+    button->depressed = false;
+    button->mixValue = 0.9f;
+    
+    if (button->mouseDownInBounds == true) {
+        if (button->onClick != NULL) {
+            button->onClick(button->sliceId, button->sliceForward);
+        }
+    }
+    
+    button->mouseDownInBounds = false;
+}
+
+void ButtonOnMouseDown (GUIButton *button) {
+    if (button == NULL) return;
+    
+    if (button->visible == false || button->enabled == false) return;
+    
+    button->mixValue = 0.5f;
+    button->mouseDownInBounds = true;
+    button->depressed = true;
+}
+
+void ButtonOnMouseEnter (GUIButton *button) {
+    if (button == NULL) return;
+    if (button->visible == false || button->enabled == false) return;
+    
+    button->highlighted = true;
+    button->mixValue = 0.9f;
+    if (button->onHighlight != NULL) {
+        button->onHighlight();
+    }
+}
+
+void ButtonOnMouseExit (GUIButton *button) {
+    if (button == NULL) return;
+    if (button->visible = false || button->enabled == false) return;
+    
+    button->depressed = false;
+    button->highlighted = false;
+    button->mouseDownInBounds = false;
+    button->mixValue = 0.7f;
+}
