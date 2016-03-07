cd0adefdb5a7ea6b8945c70076adf7bcb45bb725
Sean P <codeMonkey760@hotmail.com>
2016-03-07 13:37:57 -0800

    Worked on button programing and laying out the interface

--- src/Source/GUI.c
+++ src/Source/GUI.c
@@ -34,10 +34,66 @@ void _BuildButtons (void) {
     cb.sliceForward = false;
     gui.buttons[0] = cb;
     
+	//top slice left
     SetPRD(&cb,0.1f, 0.3f, 0.1f, 0.1f, 270.0f);
+    cb.sliceId = 3;
+	cb.sliceForward = true;
+	guiButtons[1] = cb;
+
+	//bottom slice down
+	SetPRD(&cb, 0.1f, 0.7f, 0.1f, 0.1f, 270.0f);
+	cb.sliceId = 2;
+	cb.sliceForward = false;
+	guiButtons[2] = cb;
+
+	//left slice down
+	SetPRD(&cb, 0.3f, 0.9f, 0.1f, 0.1f, 180.0f);
+	cb.sliceId = 0;
+	cb.sliceForward = true;
+	guiButtons[3] = cb;
     
-    
-    
+    //right slice down
+	SetPRD(&cb, 0.7f, 0.9f, 0.1f, 0.1f, 180.0f);
+	cb.sliceId = 1;
+	cb.sliceForward = false;
+	guiButtons[4] = cb;
+
+	//bottom slice right
+	SetPRD(&cb, 0.9f, 0.7f, 0.1f, 0.1f, 90.0f);
+	cb.sliceId = 2;
+	cb.sliceForward = true;
+	guiButtons[5] = cb;
+
+	// top slice right
+	SetPRD(&cb, 0.9f, 0.3f, 0.1f, 0.1f, 90.0f);
+	cb.sliceId = 3;
+	cb.sliceForward = false;
+	guiButtons[6] = cb;
+
+	//right slice up
+	SetPRD(&cb, 0.7f, 0.1f, 0.1f, 0.1f, 0.0f);
+	cb.sliceId = 1;
+	cb.sliceForward = true;
+	guiButtons[7] = cb;
+
+	// forward slice left
+	SetPRD(0.4f, 0.9f, 0.1f, 0.1f, 0.0f);
+	cb.texId = GetTextureByName("rot.png");
+	cb.sliceId = 4;
+	cb.sliceForward = true;
+	guiButtons[8] = cb;
+
+	// forward slice right
+	SetPRD(&cb, 0.6f, 0.9f, 0.1f, 0.1f, 0.0f);
+	cb.sliceId = 4;
+	cb.sliceForward = false;
+	guiButtons[9] = cb;
+
+	// invert the whole cube
+	SetPRD(&cb, 0.5f, 0.1f, 0.1f, 0.1f, 0.0f);
+	cb.sliceId = 6;
+	cb.sliceForward = true;
+	guiButtons[10] = cb;
 }
 
 void Render (void) {
